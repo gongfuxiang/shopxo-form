be148085197b7188119bfe0fb9b97cec319f3436
于肖磊 <18851179580@163.com>
2025-08-26 09:52:08 +0800

    修改地址显示

--- src/components/model-address/index.vue
+++ src/components/model-address/index.vue
@@ -3,7 +3,7 @@
         <div class="form-group" :style="common_store.layout_style">
             <form-title :value="props.value"></form-title>
             <div class="content w flex-1">
-                <div class="flex-col gap-10 align-c" :style="frame_style + 'height: 100%;'">
+                <div class="flex-col gap-10">
                     <el-cascader v-model="form.form_value" :options="common_store.address_list" :class="`border-focus ${ frame_class }`" :style="frame_style + style_container" :props="{ 'value': 'id', 'label': 'name', 'children': 'items' }" filterable @change="data_check" />
                     <template v-if="form.address_type == 'detailed'">
                         <el-input v-model="form.detailed_value" type="textarea" class="border-focus" :style="frame_style + style_container + 'height:100%;'" :autosize="{ minRows: 4, maxRows: 8 }" placeholder="请输入详细地址"></el-input>
